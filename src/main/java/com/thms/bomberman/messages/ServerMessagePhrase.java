efd1c23cdc2cfaba17d235fbf07ad26a23074cab
VUDHIDEJ.D <vudhidej.d@ku.th>
2017-10-29 07:20:20 +0700

    Rename class

---
+++ src/main/java/com/thms/bomberman/messages/ServerMessagePhrase.java
@@ -0,0 +1,5 @@
+package com.thms.bomberman.messages;
+
+public enum ServerMessagePhrase {
+    CONNECTING, DISCONNECTING, MOVE_RIGHT, MOVE_LEFT, MOVE_UP, MOVE_DOWN, PLACE_BOMB, POWERUP_SPAWN, POWERUP, PLAYER_SPAWN
+}
